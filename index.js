f0fbec946ac4b697d0c86d7f12ad9104a66e28a4
midhruvjaink <blivefoundations@gmail.com>
2017-06-14 23:13:25 +0530

    gitignore add and top tracks building

--- index.js
+++ index.js
@@ -15,43 +15,6 @@ function boot() {
   win.loadURL(`http://127.0.0.1:1234`)
 }
 
-/*var server = http.createServer(function(req, res) {
-  var filePath = '.' + req.url;
-    if (filePath == './')
-        filePath = './index.html';
-
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.min.js':
-            contentType = 'text/javascript';
-            break;
-        case '.min.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
-
-
-  res.writeHead(200, {'Content-Type':contentType});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(res);
-  //res.end(`file://${__dirname}/index.html`)
-})
-
-server.listen(1234, '127.0.0.1');
-*/
 http.createServer(function (request, response) {
     console.log('request starting...');
 
